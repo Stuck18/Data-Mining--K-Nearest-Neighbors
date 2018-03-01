1d872a96782ff10b2bab05827dcd0544988c36f3
Tucker <srt1457@live.missouristate.edu>
2018-03-01 13:35:54 -0600

    Continued work on KNN algorithm

--- src/KNN.py
+++ src/KNN.py
@@ -1,4 +1,59 @@
 '''
 Seth Tucker
 2/9/2018
+
+Input:
+	T = training data
+	K = number of neighbors
+	t = input tuple to classify
+	
+Output:
+	c = class to which t is assigned
+	
+KNN Algorithm
+N = empty set
+for each d in T do
+	if |N| <= K, then
+		N = N union {d};
+	else
+		if there exists a u in N such that sim(t, u) <= sim(t, d), then
+			begin
+				N = N - {u};
+				N = N union {d};
+			end
+c = class to which the most u in N are classified
+
 '''
+
+import csv # For loading the training and testing data
+import math # For finding Euclidean distance
+
+#Read data from csv file
+def getData(inputFile):
+	data = []
+	
+	with open(inputFile, 'r') as csvInput:
+		lines = csv.reader(csvInput)
+		next(lines) # Skip over header line
+		for row in lines:
+			data.append(row)
+	
+	return data
+	
+def euclideanDist(
+	
+def KNN(data):
+	#stuff and magic
+	
+def main():
+	# File locations of testing and training csv files
+	trainCSVFile = '../MNIST_train.csv'
+	testCSVFile = '../MNIST_test.csv'
+	
+	# Get data from csv files
+	trainingData = getData(trainCSVFile)
+	testingData = getData(testCSVFile)
+	
+	
+			
+main()
