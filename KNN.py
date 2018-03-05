1d8967a1002ba994e01230de5c36f3178154eed0
Tucker <srt1457@live.missouristate.edu>
2018-03-05 11:04:00 -0600

    Continued work on KNN.

--- KNN.py
+++ KNN.py
@@ -35,25 +35,52 @@ def getData(inputFile):
 		lines = csv.reader(csvInput)
 		next(lines) # Skip over header line
 		for row in lines:
-			data.append(row)
+			data.append(row[1:])
 	return data
+	
+# Compute the euclidean distance between 2 tuples
+def euclideanDist(tuple1, tuple2):
+	sum = 0
+	distance = 0 
+	for i in range(0, len(tuple1)):
+		sum += ((int(tuple1[i]) - int(tuple2[i])) * (int(tuple1[i]) - int(tuple2[i])))
+	distance = math.sqrt(sum)
+	return distance
+	
 	
 # T = Training Data, K = number of neighbors, t = input tuple to classify
 def KNN(T, K, t):
 	N = []
+	NDistances = []
 	for d in T:
 		if (len(N) <= K):
 			N.append(d)
 		else:
-			if #there exists u in N such that sim(t, u) <= sim(t, d):
-				for elem in N:
-					if (N[elem] == u):
-						N.remove(u)
-						N.append(d)
-	c = #class to which the most u in N are classified
-	return c
-			
-	file.close()
+			for elem in range(0, len(N)):
+				NDistances[elem].append(euclideanDist(t, N[elem]))
+			for elem in range(0, len(NDistances)):
+				if (NDistances[elem] <= euclideanDist(t, d)):
+				#if (euclideanDist(t, N[elem]) <= euclideanDist(t, d)):
+					N.remove(N[elem])
+					N.append(d)
+					break
+					
+	
+	votes = {}
+	for elem in range(0, len(N)):
+		if not elem in votes.keys():
+			votes[elem] = []
+	
+	for elem in range(0, len(N)):
+		distance = euclideanDist(t, N[elem])
+		if (distance != 0):
+			print(distance)
+			#votes[elem] += (1 / distance)
+		else:
+			votes[elem] += 1
+	print(votes)
+	#c = #class to which the most u in N are classified
+	#return c
 	
 # Determine the accuracy of the algorithm
 def calcAccuracy(CCList, DCList):
@@ -69,8 +96,8 @@ def calcAccuracy(CCList, DCList):
 	
 def main():
 	# File locations of testing and training csv files
-	trainCSVFile = '../MNIST_train.csv'
-	testCSVFile = '../MNIST_test.csv'
+	trainCSVFile = 'MNIST_train.csv'
+	testCSVFile = 'MNIST_test.csv'
 	
 	# Get data from csv files
 	trainingData = getData(trainCSVFile)
@@ -84,7 +111,7 @@ def main():
 	
 	print("K = ", neighbors)
 	
-	for elem in testingdata:
+	for elem in range(0, len(testingData)):
 		computedClass = KNN(trainingData, neighbors, testingData[elem])
 		desiredClass = testingData[elem][0]
 		computedClassList.append(computedClass)
